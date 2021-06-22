4f059e72612982484a76827edda0c6b1c8166e17
LevonAr <levonrm10@gmail.com>
2021-06-21 20:34:28 -0700

    Initialize Search Component Repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Header, List } from "semantic-ui-react";
+import Example from "./components/search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = ({ children }) => (
+  <Container style={{ margin: 20 }}>
+    <Header as="h3">This example is powered by Semantic UI React  😊</Header>
+    <List bulleted>
+      <List.Item
+        as="a"
+        content="💌 Official documentation"
+        href="https://react.semantic-ui.com/"
+        target="_blank"
+      />
+      <List.Item
+        as="a"
+        content="💡 StackOverflow"
+        href="https://stackoverflow.com/questions/tagged/semantic-ui-react?sort=frequent"
+        target="_blank"
+      />
+    </List>
+
+    {children}
+  </Container>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Example from "./components/search";
+
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Example />
+    </App>
   </React.StrictMode>,
   document.getElementById('root')
 );
